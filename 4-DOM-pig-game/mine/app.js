959a768c870bee2534c3efef3eb0bf75ae910000
VitorMartin <vitor.martin.simoni@gmail.com>
2020-04-09 12:00:59 -0300

    Challenge 1: two 6 in a row

--- 4-DOM-pig-game/mine/app.js
+++ 4-DOM-pig-game/mine/app.js
@@ -7,10 +7,18 @@ GAME RULES:
 - The player can choose to 'Hold', which means that his ROUND score gets added to his GLBAL score. After that, it's the next player's turn
 - The first player to reach 100 points on GLOBAL score wins the game
 
+
+CHALLENGES:
+
+Change the game to follow these rules:
+
+1. A player looses his ENTIRE score when he rolls two 6 in a row. After that, it's the next player's turn. (Hint: Always save the previous dice roll in a separate variable)
+2. Add an input field to the HTML where players can set the winning score, so that they can change the predefined score of 100. (Hint: you can read that value with the .value property in JavaScript. This is a good oportunity to use google to figure this out :)
+3. Add another dice to the game, so that there are two dices now. The player looses his current score when one of them is a 1. (Hint: you will need CSS to position the second dice, so take a look at the CSS code for the first one.)
+
 */
 
-var gamePlaying, scores, roundScore, activePlayer, dice;
-var winScore = 20;
+var gamePlaying, scores, roundScore, activePlayer, dice, previousDice, winScore;
 var diceDOM = document.querySelector('.dice');
 
 init();
@@ -27,14 +35,22 @@ document.querySelector('.btn-roll').addEventListener('click', function(){
         diceDOM.style.display = 'block';
         diceDOM.src = 'dice-' + dice + '.png';
 
-        // 3. Update the round score IF the dice was NOT a 1
-        if(dice !== 1){
-            roundScore += dice;
-            document.querySelector('#current-' + activePlayer).textContent = roundScore;
+        // Challenge 1: two 6 in a row
+        // 3. Next player IF dice = 1 or two 6 in a row
+        if(dice === 1){
+            nextPlayer();
+        }
+
+        else if (dice === 6 && previousDice === 6){
+            scores[activePlayer] = 0;
+            document.querySelector('#score-' + activePlayer).textContent = '0';
+            nextPlayer();
         }
 
         else{
-            nextPlayer();
+            previousDice = dice;
+            roundScore += dice;
+            document.querySelector('#current-' + activePlayer).textContent = roundScore;
         }
     }
 });
@@ -68,6 +84,8 @@ document.querySelector('.btn-hold').addEventListener('click', function(){
 document.querySelector('.btn-new').addEventListener('click', init);
 
 function nextPlayer(){
+    previousDice = 0;
+
     activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
 
     roundScore = 0;
@@ -82,6 +100,11 @@ function nextPlayer(){
 }
 
 function init(){
+    console.log('\nCalling init()\n');
+
+    // Challenge 1: two 6 in a row
+    previousDice = 0;
+
     gamePlaying = true;
     scores = [0,0];
     activePlayer = 0;
@@ -103,6 +126,9 @@ function init(){
     document.querySelector('.player-1-panel').classList.remove('active');
     
     document.querySelector('.player-0-panel').classList.add('active');
+
+    console.log('\n');
+    
 }
 
 
