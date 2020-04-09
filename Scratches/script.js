39c13ffde5b39f406ca2775b5ab4154a70a6daa7
VitorMartin <vitor.martin.simoni@gmail.com>
2020-04-09 13:10:49 -0300

    Create script for small scratches and tests

---
+++ Scratches/script.js
@@ -0,0 +1,9 @@
+// var previousDice = [0,0];
+// var dice1 = 0, dice2 = 0;
+
+// console.log('prevDice :', previousDice);
+// console.log('dice1 :', dice1);
+// console.log('dice2 :', dice2);
+
+// var bool = (previousDice.indexOf(dice1) !== -1) || (previousDice.indexOf(dice2)!== -1);
+// console.log('bool :', bool);
