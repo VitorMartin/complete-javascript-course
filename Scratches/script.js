d6e94999bb33934e849c1ddc7e2b99d25ba9d83a
VitorMartin <vitor.martin.simoni@gmail.com>
2020-04-20 16:02:07 -0300

    Destructuring

--- Scratches/script.js
+++ Scratches/script.js
@@ -1,16 +1,41 @@
 // var previousDice = [0,0];
 // var dice1 = 0, dice2 = 0;
 
+// ---------------------------------- //
+
 // console.log('prevDice :', previousDice);
 // console.log('dice1 :', dice1);
 // console.log('dice2 :', dice2);
 
+// ---------------------------------- //
+
 // var bool = (previousDice.indexOf(dice1) !== -1) || (previousDice.indexOf(dice2)!== -1);
 // console.log('bool :', bool);
+
+// ---------------------------------- //
 
 // var x = parseInt('-1');
 // console.log(x);
 // console.log(typeof x);
 
+// ---------------------------------- //
+
 // var x = 1234567.89;
 // console.log(x.toLocaleString()); // x.toLocaleString(locales, options) --> locales: Language Tags (BCP 47) ~~ 'en-US', 'pt'BR' etc
+
+// ---------------------------------- //
+
+// var f = function(){
+//     return [1, 2];
+// }
+
+// let a,b;
+// [a, b] = f();
+// console.log('a :', a);
+// console.log('b :', b);
+
+// a = 4;
+
+// console.log('a :', a);
+
+// ---------------------------------- //
