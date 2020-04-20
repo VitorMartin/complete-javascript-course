6509b43e4bf9578e5765db184355a4ac3dfa9a6e
VitorMartin <vitor.martin.simoni@gmail.com>
2020-04-20 14:47:50 -0300

    number.toLocaleString(locale, options);

--- Scratches/script.js
+++ Scratches/script.js
@@ -11,3 +11,6 @@
 // var x = parseInt('-1');
 // console.log(x);
 // console.log(typeof x);
+
+// var x = 1234567.89;
+// console.log(x.toLocaleString()); // x.toLocaleString(locales, options) --> locales: Language Tags (BCP 47) ~~ 'en-US', 'pt'BR' etc
